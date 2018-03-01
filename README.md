99e4c15c411c752283405c1d39feb3b111ae1f65
Wo1shishen <1991269225@qq.com>
2018-03-01 13:30:52 +0800

    再次更新

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # managementSystem
 利用NodeJs express搭建的后台管理系统
+# port 3000
+localhost:3000
--- database.js
+++ database.js
@@ -1,2 +1,2 @@
 var monogoose = require('mongoose');
-monogoose.connect("mongodb://localhost:27017/jiujiulg")
+monogoose.connect("mongodb://localhost:27017/jiujiulg",{useMongoClient:true})
